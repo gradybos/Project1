bfe2cc251998b90ad98b55dc04d3634ce8638263
gradybos <bosankog@union.edu>
2025-01-14 20:48:43 +0000

    v1.1

--- main.cpp
+++ main.cpp
@@ -25,13 +25,12 @@ bool canIgnite = false;
 void inputsInit();
 void outputsInit();
 
-void alarmActivationUpdate();
-void alarmDeactivationUpdate();
+void checkIgnition();
+void onIgnition();
 
 void welcomeMessage();
 void engineStartMessage();
-void uartTask();
-void availableCommands();
+void errorMessage();
 
 //=====[Main function, the program entry point after power on or reset]========
 
@@ -41,21 +40,14 @@ int main()
     outputsInit();
     welcomeMessage();
     while (true) {
-        alarmActivationUpdate();
-        alarmDeactivationUpdate();
-        uartTask();
+        checkIgnition();
+        if (ignitionButton) {
+            onIgnition();
+        }
     }
 }
 
 //=====[Implementations of public functions]===================================
-
-void welcomeMessage() {
-    uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48 );
-}
-
-void engineStartMessage() {
-    uartUsb.write( "Engine started.\r\n", 17 );
-}
 
 void inputsInit()
 {
@@ -71,41 +63,46 @@ void outputsInit()
     engineLed = OFF;
 }
 
+void welcomeMessage() {
+    uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48 );
+}
+
+void engineStartMessage() {
+    uartUsb.write( "Engine started.\r\n", 17 );
+}
+
+void errorMessage() {
+    if (!driverSeatButton) {
+        uartUsb.write( "Engine started.\r\n", 17 );
+    }
+    if (!driverBeltButton) {
+        uartUsb.write( "Engine started.\r\n", 17 );
+    }
+    if (!passSeatButton) {
+        uartUsb.write( "Engine started.\r\n", 17 );
+    }
+    if (!passBeltButton) {
+        uartUsb.write( "Engine started.\r\n", 17 );
+    }
+}
+
 void checkIgnition() {
-    if (canIgnite) {
+    if (driverSeatButton && driverBeltButton && passSeatButton && passBeltButton ) {
+        canIgnite = true;
         ignitionLed = ON;
     }
-    if (canIgnite && ignitionButton) {
+    else {
+        canIgnite = false;
+    }
+}
+
+void onIgnition() {
+    if (canIgnite) {
         ignitionLed = OFF;
         engineLed = ON;
         engineStartMessage();
     }
-    else if (!canIgnite && ignitionButton) {
-
+    else {
+        errorMessage();
     }
-}
-
-void uartTask(){
-	char receivedChar = '\0';
-	if( uartUsb.readable() ) {
-		uartUsb.read( &receivedChar, 1 );
-		if ( receivedChar == '2') {
-			if ( alarmState ) {
-				uartUsb.write( "The alarm is activated\r\n", 24 );
-			} 
-            else {
-				uartUsb.write( "The alarm is not activated\r\n", 28 );
-			}
-		} 
-        else if ( receivedChar == '1') {
-			availableCommands();
-		}
-	}
-}
-
-
-void availableCommands()
-{
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '2' to get the alarm state\r\n\r\n", 36 );
 }
